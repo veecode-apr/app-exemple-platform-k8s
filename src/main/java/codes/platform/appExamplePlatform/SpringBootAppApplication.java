791e7e779351faf8a9177cd5ee7db61c670de32e
Jesus Ruescas Junior <jesus.junior.1999@hotmail.com>
2023-03-17 13:44:38 -0300

    Docker compose

--- src/main/java/codes/platform/appExamplePlatform/SpringBootAppApplication.java
+++ src/main/java/codes/platform/appExamplePlatform/SpringBootAppApplication.java
@@ -3,12 +3,26 @@ package codes.platform.appExamplePlatform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class SpringBootAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootAppApplication.class, args);
+	}
+	
+	@RestController
+	@RequestMapping("/")
+	class K8sController {
+
+		@GetMapping
+		public ResponseEntity<String> get() {
+			return ResponseEntity.ok("Hello from K8S Application");
+		}
 	}
 
 }
